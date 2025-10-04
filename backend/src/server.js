baf60a84f63ef2ef1680b6daee3262910b6ac8d8
Nayan Gaikwad <gaikwadnayan2004@gmail.com>
2025-10-04 23:08:14 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
+import { serve } from "inngest/express";
 
 import { ENV } from "./config/env.js";
 
@@ -11,6 +12,11 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.get("/",(req,res)=>{
     res.send("hello world");
@@ -18,7 +24,20 @@ app.get("/",(req,res)=>{
 
 console.log(ENV.MONGO_URI);
 
-app.listen(ENV.PORT,()=>{
-    console.log(`server is running on ${ENV.PORT}`);
-    connectDB();
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
+
+export default app;
